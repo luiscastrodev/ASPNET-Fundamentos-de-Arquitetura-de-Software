70d2f4c96584debca92ccde01d92d5d2c2eb685f
luis <luiscastrodev@gmail.com>
2021-02-11 21:49:35 -0300

    estrutura projeto OOP

---
+++ OOP/01 - Classe x Objeto/Casa.cs
@@ -0,0 +1,29 @@
+﻿namespace OOP
+{
+    // Definição de Classe
+    //estrutura de codigo que tem a funcao de mapear algo do mundo real
+    public class Casa
+    {
+        public int TamanhoM2 { get; set; }
+        public int Andares { get; set; }
+        public decimal Valor { get; set; }
+        public int NumeroVagas { get; set; }
+    }
+
+    public class Objeto
+    {
+        //Objeto é quando tem uma classe sendo instanciada , ela fica alocada na memoria ela passa a ser um objeto 
+        //apesar de ser uma classe temos um objeto com valores
+        public Objeto()
+        {
+            // Definição de Objeto
+            var casa = new Casa
+            {
+                TamanhoM2 = 100,
+                Andares = 2,
+                Valor = 100000,
+                NumeroVagas = 3
+            };
+        }
+    }
+}
---
+++ OOP/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Pessoa
+    {
+        //estado seria os atributos
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        // o comportamento é quando gera uma nova informacao atravez da propria classe
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+            return idade;
+        }
+    }
+}
